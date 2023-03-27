640333e1207e2f5b6c6dfe01b211caa6d0ebe1c8
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-03-27 23:32:49 +0100

    Update HTML

--- Template/config/webhook.php
+++ Template/config/webhook.php
@@ -1,7 +1,8 @@
+<!-- KanboardSupport -->
 <div class="page-header">
-    <h2><?= t('Webhook settings') ?></h2>
+    <h2 class=""><?= t('Webhook settings') ?></h2>
 </div>
-<form method="post" action="<?= $this->url->href('ConfigController', 'save', array('redirect' => 'webhook')) ?>" autocomplete="off">
+<form method="post" class="" action="<?= $this->url->href('ConfigController', 'save', array('redirect' => 'webhook')) ?>" autocomplete="off">
     <?= $this->form->csrf() ?>
 
     <?= $this->form->label(t('Webhook URL'), 'webhook_url') ?>
@@ -13,8 +14,16 @@
 </form>
 
 <div class="page-header margin-top">
-    <h2><?= t('Webhook token') ?></h2>
+    <h2 class=""><?= t('Webhook token') ?></h2>
 </div>
+<section class="message error cleaner-warning">
+            <header></header>
+            <i class=""></i>
+            <h3 class="">
+                <span class="message-title"><?= t('Warning') ?></span>
+                <span class="message-text"><?= t('Resetting the webhook token may need to be manually updated in the cron background jobs') ?></span>
+            </h3>
+        </section>
 <div class="panel">
     <?= t('Webhook token:') ?>
     <strong><?= $this->text->e($values['webhook_token']) ?></strong>
