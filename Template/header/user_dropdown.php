8bf8cf79c1f3cf6d38a08cea81703b062a1bd5e7
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-04-15 02:52:28 +0100

    FIX: User Dropdown CSS Target

--- Template/header/user_dropdown.php
+++ Template/header/user_dropdown.php
@@ -1,3 +1,3 @@
-<li class="">
+<li class="support-dropdown">
     <?= $this->url->icon('question-circle', t('Technical Information'), 'TechnicalSupportController', 'show', ['plugin' => 'KanboardSupport', ]) ?>
 </li>
