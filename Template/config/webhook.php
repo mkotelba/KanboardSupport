892942241e593d6dbbecc1dd45a57e9c0879aaa6
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-08-27 22:41:25 +0100

    FIX: Webhook Translations
     - #16

--- Template/config/webhook.php
+++ Template/config/webhook.php
@@ -1,6 +1,6 @@
 <!-- KanboardSupport Plugin -->
 <div class="page-header">
-    <h2 class=""><?= t('Webhook settings') ?></h2>
+    <h2 class=""><?= t('Webhook Settings') ?></h2>
 </div>
 <form method="post" class="" action="<?= $this->url->href('ConfigController', 'save', array('redirect' => 'webhook')) ?>" autocomplete="true">
     <?= $this->form->csrf() ?>
@@ -14,19 +14,19 @@
 </form>
 
 <div class="page-header margin-top">
-    <h2 class=""><?= t('Webhook token') ?></h2>
+    <h2 class=""><?= t('Webhook Token') ?></h2>
 </div>
 <section class="message error cleaner-warning">
             <header></header>
             <i class=""></i>
             <h3 class="">
                 <span class="message-title"><?= t('Warning') ?></span>
-                <span class="message-text"><?= t('Resetting the webhook token may need to be manually updated in the cron background jobs') ?></span>
+                <span class="message-text"><?= t('Resetting the webhook token may require the cron background jobs to be manually updated') ?></span>
             </h3>
         </section>
 <div class="panel">
-    <?= t('Webhook token:') ?>
+    <?= t('Token:') ?>
     <strong><?= $this->text->e($values['webhook_token']) ?></strong>
 </div>
 
-<?= $this->url->link(t('Reset token'), 'ConfigController', 'token', array('type' => 'webhook'), true, 'btn btn-red') ?>
+<?= $this->url->link(t('Reset Token'), 'ConfigController', 'token', array('type' => 'webhook'), true, 'btn btn-red') ?>
