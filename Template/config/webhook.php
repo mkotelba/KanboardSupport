5d1fbb5cbf05af95028bfdd4842d813cb0ae02e5
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-09-07 21:58:42 +0100

    FIX: Migrate Icons for Improved Performance
     - Resolves #15

--- Template/config/webhook.php
+++ Template/config/webhook.php
@@ -1,32 +1,34 @@
 <!-- KanboardSupport Plugin -->
-<div class="page-header">
-    <h2 class=""><?= t('Webhook Settings') ?></h2>
-</div>
-<form method="post" class="" action="<?= $this->url->href('ConfigController', 'save', array('redirect' => 'webhook')) ?>" autocomplete="true">
-    <?= $this->form->csrf() ?>
-
-    <?= $this->form->label(t('Webhook URL'), 'webhook_url') ?>
-    <?= $this->form->text('webhook_url', $values, $errors) ?>
-
-    <div class="form-actions">
-        <button type="submit" class="btn btn-blue"><?= t('Save') ?></button>
+<div class="webhooks-page">
+    <div class="page-header" style="margin-top: 10px;">
+        <h2 class=""><span class="webhooks-icon"></span> <?= t('Webhook Settings') ?></h2>
     </div>
-</form>
+    <form method="post" class="" action="<?= $this->url->href('ConfigController', 'save', array('redirect' => 'webhook')) ?>" autocomplete="true">
+        <?= $this->form->csrf() ?>
 
-<div class="page-header margin-top">
-    <h2 class=""><?= t('Webhook Token') ?></h2>
-</div>
-<section class="message error cleaner-warning">
-            <header></header>
-            <i class=""></i>
-            <h3 class="">
-                <span class="message-title"><?= t('Warning') ?></span>
-                <span class="message-text"><?= t('Resetting the webhook token may require the cron background jobs to be manually updated') ?></span>
-            </h3>
-        </section>
-<div class="panel">
-    <?= t('Token:') ?>
-    <strong><?= $this->text->e($values['webhook_token']) ?></strong>
-</div>
+        <?= $this->form->label(t('Webhook URL'), 'webhook_url') ?>
+        <?= $this->form->text('webhook_url', $values, $errors) ?>
 
-<?= $this->url->link(t('Reset Token'), 'ConfigController', 'token', array('type' => 'webhook'), true, 'btn btn-red') ?>
+        <div class="form-actions">
+            <button type="submit" class="btn btn-blue"><?= t('Save') ?></button>
+        </div>
+    </form>
+
+    <div class="page-header margin-top">
+        <h2 class=""><?= t('Webhook Token') ?></h2>
+    </div>
+    <section class="message error cleaner-warning">
+                <header></header>
+                <i class=""></i>
+                <h3 class="">
+                    <span class="message-title"><?= t('Warning') ?></span>
+                    <span class="message-text"><?= t('Resetting the webhook token may require the cron background jobs to be manually updated') ?></span>
+                </h3>
+            </section>
+    <div class="panel">
+        <?= t('Token:') ?>
+        <strong><?= $this->text->e($values['webhook_token']) ?></strong>
+    </div>
+
+    <?= $this->url->link(t('Reset Token'), 'ConfigController', 'token', array('type' => 'webhook'), true, 'btn btn-red') ?>
+</div>
