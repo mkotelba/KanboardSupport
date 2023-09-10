6e8dcf9440cc53ab3b5b7d391766948bcf5ff598
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-09-10 21:53:59 +0100

    Add Webhook Information Page
     - #18

--- Controller/TechnicalSupportController.php
+++ Controller/TechnicalSupportController.php
@@ -5,9 +5,11 @@ namespace Kanboard\Plugin\KanboardSupport\Controller;
 use Kanboard\Controller\BaseController;
 
 /**
- * Class Technical Support
+ * Plugin KanboardSupport
  *
- * @author aljawaid
+ * Class TechnicalSupportController
+ * @package  Kanboard/Controller
+ * @author   aljawaid
  */
 class TechnicalSupportController extends \Kanboard\Controller\ConfigController
 {
@@ -23,6 +25,18 @@ class TechnicalSupportController extends \Kanboard\Controller\ConfigController
             'db_version' => $this->db->getDriver()->getDatabaseVersion(),
             'user_agent' => $this->request->getServerVariable('HTTP_USER_AGENT'),
             'title' => e('Settings %s Configuration', ' &#10562; '),
+        )));
+    }
+
+    /**
+     * Display the Webhooks Information Page
+     *
+     * @access public
+     */
+    public function showWebhookInformation()
+    {
+        $this->response->html($this->helper->layout->config('kanboardSupport:config/webhook_information', array(
+            'title' => e('Webhooks %s Information', ' &#10562; '),
         )));
     }
 }
