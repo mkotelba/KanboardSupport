4dadf61fb281ec048ed1abedff51e82ab9eda764
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-09-11 09:37:34 +0100

    Add Content - Supported Events
     - #18

--- Template/config/webhook_information.php
+++ Template/config/webhook_information.php
@@ -1,1 +1,29 @@
+<!-- KanboardSupport Plugin -->
+<div class="webhooks-info-page">
+    <section class="">
+        <div id="SupportedEvents" class="page-header" style="margin-top: 10px;">
+            <h2 class=""><span class="webhooks-icon"></span> <?= t('Supported Events') ?></h2>
+        </div>
+        <ul class="">
+            <li class=""><code>comment.create</code></li>
+            <li class=""><code>comment.update</code></li>
+            <li class=""><code>comment.delete</code></li>
+            <li class=""><code>file.create</code></li>
+            <li class=""><code>task.move.project</code></li>
+            <li class=""><code>task.move.column</code></li>
+            <li class=""><code>task.move.position</code></li>
+            <li class=""><code>task.move.swimlane</code></li>
+            <li class=""><code>task.update</code></li>
+            <li class=""><code>task.create</code></li>
+            <li class=""><code>task.close</code></li>
+            <li class=""><code>task.open</code></li>
+            <li class=""><code>task.assignee_change</code></li>
+            <li class=""><code>subtask.update</code></li>
+            <li class=""><code>subtask.create</code></li>
+            <li class=""><code>subtask.delete</code></li>
+            <li class=""><code>task_internal_link.create_update</code></li>
+            <li class=""><code>task_internal_link.delete</code></li>
+        </ul>
+    </section>
+</div>
 
