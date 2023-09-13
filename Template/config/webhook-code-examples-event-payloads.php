d5aaef6876b0a7373ce56bd0a3cf0ab3534af76d
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-09-13 08:03:29 +0100

    Fix Syntax - Example of HTTP Request
     - #18

--- Template/config/webhook-code-examples-event-payloads.php
+++ Template/config/webhook-code-examples-event-payloads.php
@@ -2,7 +2,7 @@
     <?= t('All event payloads are in the following format:') ?>
 </p>
 <div class="panel">
-<pre class="">
+    <pre class="">
 {
     "event_name": "model.event_name",
     "event_data": {
@@ -10,7 +10,7 @@
         "key2": "value2"
     }
 }
-</pre>
+    </pre>
 </div>
 <p class="">
     <?= e('The %s values are not necessary normalized across events.', '<strong><code>event_data</code></strong>') ?>
