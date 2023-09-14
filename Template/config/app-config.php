252796953d63ea3f10e6b7c6fc79815ee422f988
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-09-14 06:12:44 +0100

    NEW: Create Plugins Configuration Accordion
     - migrate values - #19

---
+++ Template/config/app-config.php
@@ -0,0 +1,70 @@
+<details class="accordion-section app-config">
+    <summary class="accordion-title">
+        <?= t('Plugins Configuration') ?>
+    </summary>
+    <div class="accordion-content">
+        <span class="data-wrap">
+            <li class="app-info-title"><?= t('Plugins Installer') ?></li>
+            <li class="app-info-value border-bottom-thick">
+                <?php if (PLUGIN_INSTALLER == false): ?>
+                    <?= t('Disabled') ?>
+                <?php else: ?>
+                    <?= t('Enabled') ?>
+                <?php endif ?>
+            </li>
+            <?php if (PLUGIN_INSTALLER == false): ?>
+                <span class="fail-x" title="<?= t('Plugins cannot be installed. This is also set by default for security reasons.') ?>">&#10008;</span>
+            <?php else: ?>
+                <span class="pass-tick" title="<?= t('Plugins can be automatically installed through the Plugins Directory') ?>">&#10004;</span>
+            <?php endif ?>
+        </span>
+        <span class="data-wrap">
+            <li class="app-info-title"><?= t('Plugins Directory') ?></li>
+            <li class="app-info-value value-path border-bottom-thick privacy"><?= PLUGINS_DIR ?></li>
+            <?php if (!is_writable(PLUGINS_DIR)): ?>
+                <span class="fail-x" title="<?= t('This directory is not writeable by the web server user') ?>">&#10008;</span>
+            <?php else: ?>
+                <span class="pass-tick" title="<?= t('This directory is writeable by the web server user') ?>">&#10004;</span>
+            <?php endif ?>
+            <?php if ($this->user->isAdmin()): ?>
+                <div id="pCheck" class="p-check">
+                    <?php if (!is_writable(PLUGINS_DIR)): ?>
+                        <span class="p-dir" title="<?= t('Directory Permissions') ?>">
+                            <?= $this->helper->supportHelper->getPermissions(PLUGINS_DIR) ?>
+                        </span>
+                        <span class="p-linux value-ip" title="<?= t('Linux Directory Permissions') ?>">
+                            <?= $this->helper->supportHelper->getPermissionsLinux(PLUGINS_DIR) ?>
+                        </span>
+                        <span class="p-owner" title="<?= t('Directory Owner') ?>">
+                            <?= $this->helper->supportHelper->getPermissionsOwner(PLUGINS_DIR) ?>
+                        </span>
+                    <?php else: ?>
+                        <span class="p-dir" title="<?= t('Directory Permissions') ?>">
+                            <?= $this->helper->supportHelper->getPermissions(PLUGINS_DIR) ?>
+                        </span>
+                        <span class="p-linux value-ip" title="<?= t('Linux Directory Permissions') ?>">
+                            <?= $this->helper->supportHelper->getPermissionsLinux(PLUGINS_DIR) ?>
+                        </span>
+                        <span class="p-owner" title="<?= t('Directory Owner') ?>">
+                            <?= $this->helper->supportHelper->getPermissionsOwner(PLUGINS_DIR) ?>
+                        </span>
+                    <?php endif ?>
+                </div>
+            <?php endif ?>
+        </span>
+        <span class="data-wrap">
+            <li class="app-info-title"><?= t('Plugins Directory URL') ?></li>
+            <?php if (PLUGIN_API_URL == 'https://kanboard.org/plugins.json'): ?>
+                <li class="app-info-value border-bottom-thick" title="<?= t('Plugins will be listed from the default source') ?>">
+                    <?= PLUGIN_API_URL ?>
+                </li>
+                <span class="pass-tick" title="<?= t('Plugins will be listed from the default source') ?>">&#10004;</span>
+            <?php else: ?>
+                <li class="app-info-value border-bottom-thick privacy" title="<?= t('Plugins will be listed from a custom source') ?>">
+                    <?= PLUGIN_API_URL ?>
+                </li>
+                <span class="pass-tick" title="<?= t('Plugins will be listed from a custom source') ?>">&#10004;</span>
+            <?php endif ?>
+        </span>
+    </div>
+</details>
