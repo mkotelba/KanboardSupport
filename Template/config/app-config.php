fd524763181de3a6182710eed3e14074b7b955e5
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-09-16 21:12:36 +0100

    NEW: Add LDAP Authentication Setting
     - #19

--- Template/config/app-config.php
+++ Template/config/app-config.php
@@ -414,8 +414,20 @@
         <?= t('LDAP Configuration') ?>
     </summary>
     <div class="accordion-content">
-        <fieldset class="http-client-proxy">
+        <fieldset class="ldap">
             <legend class=""><?= t('LDAP') ?></legend>
+            <span class="data-wrap">
+                <li class="app-info-title"><?= e('%s Authentication', '<abbr title="Lightweight Directory Access Protocol">LDAP</abbr>') ?></li>
+                <?php if (LDAP_AUTH == false): ?>
+                    <li class="app-info-value border-bottom-thick" title="<?= t('This is the default setting') ?>">
+                        <?= t('Disabled') ?>
+                    </li>
+                <?php else: ?>
+                    <li class="app-info-value border-bottom-thick">
+                        <?= t('Enabled') ?>
+                    </li>
+                <?php endif ?>
+            </span>
         </fieldset>
     </div>
 </details>
