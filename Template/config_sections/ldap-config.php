f9d7e53e666a999625b100e9b6065af1fa1bf20b
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-09-22 23:59:41 +0100

    FIX: Translation
     - #19

--- Template/config_sections/ldap-config.php
+++ Template/config_sections/ldap-config.php
@@ -1,5 +1,5 @@
 <span class="data-wrap">
-    <li class="app-info-title"><?= e('%s Authentication', '<abbr title="Lightweight Directory Access Protocol">LDAP</abbr>') ?></li>
+    <li class="app-info-title"><?= e('%s Authentication', '<abbr title="' . t('Lightweight Directory Access Protocol') . '">LDAP</abbr>') ?></li>
     <?php if (LDAP_AUTH == false): ?>
         <li class="app-info-value border-bottom-thick" title="<?= t('This is the default setting') ?>">
             <?= t('Disabled') ?>
