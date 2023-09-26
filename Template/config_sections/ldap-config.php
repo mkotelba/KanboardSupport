0292a205f43cb1313fe6001eb31da3490f364721
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-09-26 03:03:14 +0100

    Add `LDAP_GROUP_USER_FILTER`
     - #19

--- Template/config_sections/ldap-config.php
+++ Template/config_sections/ldap-config.php
@@ -239,5 +239,15 @@
                 </li>
             <?php endif ?>
         </span>
+        <span class="data-wrap">
+            <li class="app-info-title"><?= t('Group User Filter') ?></li>
+            <?php if (LDAP_GROUP_USER_FILTER != ''): ?>
+                <li class="app-info-value border-bottom-thick"><?= LDAP_GROUP_USER_FILTER ?></li>
+            <?php else: ?>
+                <li class="app-info-value border-bottom-thick not-set" title="<?= t('This is the default setting') ?>">
+                    <?= t('Not Set') ?>
+                </li>
+            <?php endif ?>
+        </span>
     </fieldset>
 <?php endif ?>
