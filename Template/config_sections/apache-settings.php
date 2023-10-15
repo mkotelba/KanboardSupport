cfed2a68fdb64a18176f3c14729dcf6e1669e3c0
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-10-15 04:10:04 +0100

    NEW: Show Apache Modules
     - #31

--- Template/config_sections/apache-settings.php
+++ Template/config_sections/apache-settings.php
@@ -7,5 +7,17 @@
             <li class="app-info-title"><?= t('Apache Version') ?></li>
             <li class="app-info-value value-version border-bottom-thick"><?= apache_get_version() ?></li>
         </span>
+        <details class="accordion-section apache-config">
+            <summary class="accordion-title">
+                <?= t('Apache Modules') ?>
+            </summary>
+            <div class="accordion-content">
+                <ul class="apache-modules">
+                    <?php foreach (apache_get_modules() as $module): ?>
+                        <li class=""><?= $this->helper->supportHelper->embedSVGIcon('apache-logo-icon') ?> <?= $module ?></li>
+                    <?php endforeach ?>
+                </ul>
+            </div>
+        </details>
     </ul>
 </div>
