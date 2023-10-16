e32f1193059824c546b3be750a81d192e78865dc
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-10-16 22:37:12 +0100

    NEW: Display `HTACCESS` File to Admin Users
     - #30

--- Template/config_sections/htaccess.php
+++ Template/config_sections/htaccess.php
@@ -3,5 +3,6 @@
         <i class="fa fa-file-code-o"></i> <?= t('Hypertext Access File') ?>
     </summary>
     <div class="accordion-content">
+        <pre class="htaccess-file"><?= file_get_contents(ROOT_DIR . DIRECTORY_SEPARATOR . '.htaccess') ?></pre>
     </div>
 </details>
