22904afaff7852163231b4db73cdea6086308906
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-10-16 20:38:06 +0100

    Convert Apache Section into Accordion
     - #31

--- Template/config_sections/apache-settings.php
+++ Template/config_sections/apache-settings.php
@@ -1,23 +1,30 @@
-<h2 class="">
-    <i class="fa fa-file-code-o"></i> <?= t('Apache Web Server Information') ?>
-</h2>
-<div class="apache-info">
-    <ul class="">
-        <span class="data-wrap">
-            <li class="app-info-title"><?= t('Apache Version') ?></li>
-            <li class="app-info-value value-version border-bottom-thick"><?= apache_get_version() ?></li>
-        </span>
-        <details class="accordion-section apache-config">
-            <summary class="accordion-title">
-                <?= t('Apache Modules') ?>
-            </summary>
-            <div class="accordion-content">
-                <ul class="apache-modules">
-                    <?php foreach (apache_get_modules() as $module): ?>
-                        <li class=""><?= $this->helper->supportHelper->embedSVGIcon('apache-logo-icon') ?> <?= $module ?></li>
-                    <?php endforeach ?>
-                </ul>
+<details class="accordion-section apache-config">
+    <summary class="accordion-title">
+        <?= $this->helper->supportHelper->embedSVGIcon('apache-logo-icon') ?> <?= t('Apache Web Server Information') ?>
+    </summary>
+    <div class="accordion-content apache-info">
+        <ul class="">
+            <span class="data-wrap">
+                <li class="app-info-title"><?= t('Apache Version') ?></li>
+                <li class="app-info-value value-version border-bottom-thick"><?= apache_get_version() ?></li>
+            </span>
+            <div class="">
+                <h4 class=""><?= t('Apache Modules') ?></h4>
+                <div class="">
+                    <ul class="apache-modules">
+                        <?php foreach (apache_get_modules() as $module): ?>
+                            <li class="">
+                                <?= $this->helper->supportHelper->embedSVGIcon('apache-logo-icon') ?>
+                                <?php if ($module == 'mod_rewrite'): ?>
+                                    <strong title="<?= t('Required for URL Rewriting') ?>"><?= $module ?></strong>
+                                <?php else: ?>
+                                    <?= $module ?>
+                                <?php endif ?>
+                            </li>
+                        <?php endforeach ?>
+                    </ul>
+                </div>
             </div>
-        </details>
-    </ul>
-</div>
+        </ul>
+    </div>
+</details>
