04319aa8f81e99fc3eed15cb6a2cd281b680c5d7
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-10-18 07:18:58 +0100

    FIX: HTACCESS Icon

--- Template/config_sections/htaccess.php
+++ Template/config_sections/htaccess.php
@@ -1,6 +1,6 @@
 <details class="accordion-section htaccess-config">
     <summary class="accordion-title">
-        <i class="fa fa-file-code-o"></i> <?= t('Hypertext Access File') ?>
+        <?= $this->helper->supportHelper->embedSVGIcon('htaccess-file-icon') ?> <?= t('Hypertext Access File') ?>
     </summary>
     <div class="accordion-content">
         <pre class="htaccess-file"><?= file_get_contents(ROOT_DIR . DIRECTORY_SEPARATOR . '.htaccess') ?></pre>
